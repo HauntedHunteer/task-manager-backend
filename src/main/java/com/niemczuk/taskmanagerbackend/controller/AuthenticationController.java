6e41d602e4c86e430e9d6387780fe32d8aef3587
HauntedHunteer <m.niemczuk@interia.pl>
2022-01-27 14:10:57 +0100

    cross-origin added

--- src/main/java/com/niemczuk/taskmanagerbackend/controller/AuthenticationController.java
+++ src/main/java/com/niemczuk/taskmanagerbackend/controller/AuthenticationController.java
@@ -5,14 +5,12 @@ import com.niemczuk.taskmanagerbackend.model.dto.authentication.AuthenticationRe
 import com.niemczuk.taskmanagerbackend.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/authenticate")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
